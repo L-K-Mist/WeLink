4f3bf44ddcdfc46e628c531aa132a86cb41d845f
LKMist <vandenbosch.dylan1@gmail.com>
2017-12-01 10:25:18 +0200

    install and start

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template dark>
+    <div id="app">
+      <div  id="example">
+        <vue-particles
+          color="#dadbe5"
+          :particleOpacity="0.7"
+          :particlesNumber="80"
+          shapeType="circle"
+          :particleSize="6"
+          linesColor="blue"
+          :linesWidth="2"
+          :lineLinked="true"
+          :lineOpacity="0.5"
+          :linesDistance="150"
+          :moveSpeed="3"
+          :hoverEffect="true"
+          hoverMode="grab"
+          :clickEffect="true"
+          clickMode="push"
+        >
+          <h1>Hello World from DillyDee</h1>
+        </vue-particles>
+      </div>
+
+    </div>
+ </template>
+
+ <style>
+#example {
+  background-image: url("/static/zen-garden-opt2.jpg");
+}
+</style>
+
