b9548b6942183f610e231fa2421cdc5b4a8ddf95
LKMist <vandenbosch.dylan1@gmail.com>
2017-12-04 21:23:05 +0200

    back to stable with particles showing (was duplicted)

--- src/App.vue
+++ src/App.vue
@@ -52,24 +52,7 @@
         </transition>
 
       </div>
-      <vue-particles
-        color="#ffffff"
-        :particleOpacity="0.7"
-        linesColor="#ffffff"
-        :particlesNumber="80"
-        shapeType="circle"
-        :particleSize="5"
-        :linesWidth="2"
-        :lineLinked="true"
-        :lineOpacity="0.4"
-        :linesDistance="150"
-        :moveSpeed="3"
-        :hoverEffect="true"
-        hoverMode="grab"
-        :clickEffect="true"
-        clickMode="push"
-      >
-      </vue-particles>
+
 
         <div class="app-front text-xs-center" style="position: absolute; top: 60%">
           <transition appear
