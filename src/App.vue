73ad115395af46ad5b89bf5ce303e2f6d5763ee1
LKMist <vandenbosch.dylan1@gmail.com>
2017-12-06 23:12:49 +0200

    stable time to branch for see under hood alert

--- src/App.vue
+++ src/App.vue
@@ -94,8 +94,12 @@
             {{ alertMessage }}
           </v-alert>
         </div>
+
+
+
         <!-- SWITCHES -->
-        <div v-if="counter > 10" style="position: absolute; left: 1%; z-index: 8">
+        <div v-if="counter > 10" style="position: absolute; left: 1%; z-index: 8"
+        transition="slide-x-transition">
           <v-layout column>
             <v-switch light
               v-model="showComingSoon">
@@ -108,9 +112,37 @@
             </v-switch>
           </v-layout>
         </div>
-
-
-        <!-- <v-switch v-bind:label="`Switch 2: ${ex12.toString()}`" v-model="ex12"></v-switch> -->
+      </div>
+      <div  style="position: absolute; top: 110%">
+        <!-- <v-container style="position: absolute; top: 150%"> -->
+          <v-layout column>
+            <v-card xs12 md9>
+              <p>We stand on the shoulders of Giants, and this website is no exception!</p>
+              <p>None of this would have been possible AND performant, without the latest developments (on top of last year&#39;s latest developments) in programming.</p>
+              <p>Big thanks to </p>
+              <hr>
+              <p>Vue for being the over-all Reactive, Performant, Front-End Framework.</p>
+              <p>Why we chose vue over React and Angular:</p>
+              <p>Although each is great, capable of producing similarly functional websites/apps; (Gmail is built on Angular, Facebook on React) we find that vue is lighter, nimbler, and independent.</p>
+              <hr>
+              <p>Vuetify for it&#39;s implementation of Google&#39;s Beautiful Material Design Spec.</p>
+              <p>Vuetify in a nutshell: Vuetify provides the button, card, and slider components generally used in this site.</p>
+              <hr>
+              <p>vue-particle for the playful Particle Component you were playing with on the Landing Page Hero Element.</p>
+              <p>Netlify for their (so far free  -  and Supa-Fast) cdn-hosting of this site.</p>
+              <p>Netlify in a nutshell: Netlify is a Helpful Static-Site Hosting solution and Content Delivery Network.</p>
+              <p>So yes, at the moment this is a server-less, <em>static</em> app, served via cdn (Content Delivery Network)</p>
+              <hr>
+              <p>Special Mention: NodeJs
+              This site is not - strictly speaking - a Nodejs application; as it doesn&#39;t run off of a NodeJs Server. However, NodeJs is so fundamental to what&#39;s happening on the web right now that we can certainly regard NodeJs as an instrumental part of bringing this site into deployed status.</p>
+              <p>NodeJS in a nutshell - [tough one, I&#39;ll have to come back to that later] sometimes you don&#39;t know what you think you know eh #smileyWink# </p>
+              <p>Okay, basically what they&#39;ve done there is brought javascript out of the browser and into the land of servers and PC&#39;s. Truly: They took Chrome&#39;s aptly-named V8 Engine, and turned it into something that you install on your PC - not exactly; but almost like a second Operating System that speaks Javascript. In essence therefore translating (AKA compiling) into C++ Native. </p>
+              <p>Yis I think this is so wrong, rather check out what better brains than mine have to say: </p>
+              <p>Okay, so who&#39;ve we left out; let me see...</p>
+              <p>[need tech quotes ticker] </p>
+            </v-card>
+          </v-layout>
+        </v-container>
       </div>
     </v-app>
   </div>
@@ -118,7 +150,7 @@
 
 <script>
 //import Foot from "./components/Foot";
-//import MainContent from "./components/Main";
+import MainContent from "./components/Main";
 import stars from "./assets/starrysky.jpg";
 import welinkLogo from "./assets/welink-logo-pwa.png";
 
