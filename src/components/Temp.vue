87b2dde5fc95da604f394d163b2d66b7d831edf7
LKMist <vandenbosch.dylan1@gmail.com>
2017-12-07 13:48:01 +0200

    trying stuff but time to simplify code-base

---
+++ src/components/Temp.vue
@@ -0,0 +1,47 @@
+<v-container>
+          <v-layout row wrap>
+            <v-container>
+
+
+              <v-flex xs11 md9 style="justify-content: center" >
+                <v-card>
+            <div>
+              <v-flex xs11 md9 class="text-xs-center">
+                <h1 id="what-s-under-the-hood">What&#39;s under the Hood</h1>
+                <p>We stand on the shoulders of Giants, and this website is no exception!</p>
+                <p>None of this would have been possible AND performant, without the latest developments (on top of last year&#39;s latest developments) in programming.</p>
+                <h1 id="soooo-big-thanks-to-">SOooo Big Thanks to:</h1>
+                <v-card>
+
+                  <p>VueJS for being the over-all Front-End Framework - the thing we plug the other frameworks into (mostly).</p>
+                  <p>Why vue? Reactive, Performant, light.</p>
+                  <p>React (as in Facebook) and Angular (as in Gmail) are great, but sometimes tight budgets result in frugal code.
+                  Plus there&#39;s an air of victory around Evan You&#39;s creation - David and Goliath vibes... although he seems to get much love from Google?  :) [use chips plus smileys]</p>
+                  <p>Although each is great, capable of producing similarly functional websites/apps; (Gmail is built on Angular, Facebook on React) we find that vue is, nimbler, and independent.</p>
+                  <hr>
+
+                </v-card>
+
+              </v-flex>
+              <p>Vuetify for it&#39;s implementation of Google&#39;s Beautiful Material Design Spec.</p>
+              <p>Vuetify in a nutshell: Vuetify provides the button, card, and slider components generally used in this site. These components follow Google&#39;s Material Design Spec. Reading through their documentation is like a crash-course in design itself - Thank Google! </p>
+              <hr>
+              <p>vue-particle for the playful Particle Component you were playing with on the Landing Page Hero Element.</p>
+              <p>Netlify for their (so far free  -  and Supa-Fast) cdn-hosting of this site.</p>
+              <p>Netlify in a nutshell: Netlify is a Helpful Static-Site Hosting solution and Content Delivery Network.</p>
+              <p>So yes, at the moment this is a server-less, <em>static</em> app, served via cdn (Content Delivery Network)</p>
+              <hr>
+              <p>Special Mention: NodeJs
+              This site is not - strictly speaking - a Nodejs application; as it doesn&#39;t run off of a NodeJs Server. However, NodeJs is so fundamental to what&#39;s happening on the web right now that we can certainly regard NodeJs as an instrumental part of bringing this site into deployed status.</p>
+              <p>NodeJS in a nutshell - [tough one, I&#39;ll have to come back to that later] sometimes you don&#39;t know what you think you know eh #smileyWink# </p>
+              <p>Okay, basically what they&#39;ve done there is brought javascript out of the browser and into the land of servers and PC&#39;s. Truly: They took Chrome&#39;s aptly-named V8 Engine, and turned it into something that you install on your PC - not exactly; but almost like a second Operating System that speaks Javascript. In essence therefore translating (AKA compiling) into C++ Native. </p>
+              <p>Yis I think this is so wrong, rather check out what better brains than mine have to say: </p>
+              <p>Okay, so who&#39;ve we left out; let me see...</p>
+              <p>[need tech quotes ticker] </p>
+            </div>
+          </v-card>
+        </v-flex>
+      </v-container>
+    </v-layout>
+  </v-container>
+</v-container>
