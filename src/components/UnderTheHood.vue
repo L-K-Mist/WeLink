973e56fe6b7ef9ae19a1bb3e6914184c815e6c42
LKMist <vandenbosch.dylan1@gmail.com>
2017-12-09 10:22:45 +0200

    The Card comes in now it just needs some tweaking.

--- src/components/UnderTheHood.vue
+++ src/components/UnderTheHood.vue
@@ -1,93 +1,44 @@
 <template>
-      <v-app id="inspire">
+  <v-app id="inspire">
     <div>
       <v-layout row justify-center>
         <v-btn color="primary" dark @click.stop="dialog = true">Open Dialog 1</v-btn>
-         <v-menu bottom offset-y>
+        <v-menu bottom offset-y></v-menu>
+        <v-dialog
+          v-model="dialog"
+          max-width="80%"
+          transition="dialog-bottom-transition"
+          :overlay="false"
+          scrollable>
 
+          <v-card>
 
-        </v-menu>
-      <v-dialog
-        v-model="dialog"
-        fullscreen
-        transition="dialog-bottom-transition"
-        :overlay=false
-        scrollable
-      >
-        <v-card>
-            <v-toolbar style="flex: 0 0 auto;" dark class="primary">
-            <v-btn icon @click.native="dialog = false" dark>
-              <v-icon>close</v-icon>
-            </v-btn>
-            <v-toolbar-title>Settings</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-toolbar-items>
-              <v-btn dark flat @click.native="dialog = false">Save</v-btn>
-              <v-menu bottom right offset-y>
-                <v-btn slot="activator" dark icon>
-                  <v-icon>more_vert</v-icon>
-                </v-btn>
+            <v-toolbar style="flex: 0 0 auto">
+              <v-btn icon @click.native="dialog = false" dark>
+                <v-icon>close</v-icon>
+              </v-btn>
+              <v-toolbar-title>Settings</v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-toolbar-items>
+                <v-btn dark flat @click.native="dialog = false">Save</v-btn>
+                <v-menu bottom right offset-y>
+                  <v-btn slot="activator" dark icon>
+                    <v-icon>more_vert</v-icon>
+                  </v-btn>
+                </v-menu>
+              </v-toolbar-items>
+            </v-toolbar>
+            <v-card-text style="opacity: 1">
+              <v-btn style="opacity: 1" color="primary" dark @click.stop="dialog2 = !dialog2">Open Dialog 2</v-btn>
+              <v-tooltip right>
+              <v-btn slot="activator">Tool Tip Activator</v-btn>
+                Tool Tip
+              </v-tooltip>
+            <v-divider></v-divider>
+            </v-card-text>
 
-              </v-menu>
-            </v-toolbar-items>
-          </v-toolbar>
-          <v-card-text>
-            <v-btn color="primary" dark @click.stop="dialog2 = !dialog2">Open Dialog 2</v-btn>
-            <v-tooltip right>
-            <v-btn slot="activator">Tool Tip Activator</v-btn>
-              Tool Tip
-            </v-tooltip>
-          <v-list three-line subheader>
-            <v-subheader>User Controls</v-subheader>
-            <v-list-tile avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>Content filtering</v-list-tile-title>
-                <v-list-tile-sub-title>Set the content filtering level to restrict apps that can be downloaded</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile avatar>
-              <v-list-tile-content>
-                <v-list-tile-title>Password</v-list-tile-title>
-                <v-list-tile-sub-title>Require password for purchase or use password to restrict purchase</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          <v-divider></v-divider>
-          <v-list three-line subheader>
-            <v-subheader>General</v-subheader>
-            <v-list-tile avatar>
-              <v-list-tile-action>
-                <v-checkbox v-model="notifications"></v-checkbox>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>Notifications</v-list-tile-title>
-                <v-list-tile-sub-title>Notify me about updates to apps or games that I downloaded</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile avatar>
-              <v-list-tile-action>
-                <v-checkbox v-model="sound"></v-checkbox>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>Sound</v-list-tile-title>
-                <v-list-tile-sub-title>Auto-update apps at any time. Data charges may apply</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile avatar>
-              <v-list-tile-action>
-                <v-checkbox v-model="widgets"></v-checkbox>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>Auto-add widgets</v-list-tile-title>
-                <v-list-tile-sub-title>Automatically add home screen widgets</v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-          </v-card-text>
-
-            <div style="flex: 1 1 auto;"></div>
-        </v-card>
-       </v-dialog>
+          </v-card>
+        </v-dialog>
       </v-layout>
     </div>
   </v-app>
