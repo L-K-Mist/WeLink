46d0c776c65618c2ea4b0cb1d474681b8d89406c
Dylan <dylan@vandenbosch.co.za>
2018-02-08 19:19:51 +0200

    moved and fixed issue of files outside of server folder

--- server/app.js
+++ server/app.js
@@ -9,6 +9,12 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/test', (req, res) => {
+    res.send({
+        message: "Hello Dylan"
+    })
+})
+
 app.post('/register', (req, res) => {
     res.send({
         message: `Hello ${req.body.email }! Your user was registered. Have fun Dylan! ;]`
